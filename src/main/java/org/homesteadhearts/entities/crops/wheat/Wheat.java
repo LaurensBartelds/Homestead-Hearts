c2f91521d40bed4c04da4197fcf33d628458fd0c
HAN-Luna <lxqrebergen@gmail.com>
2025-03-25 10:19:17 +0100

    hi

--- src/main/java/org/homesteadhearts/entities/crops/wheat/Wheat.java
+++ src/main/java/org/homesteadhearts/entities/crops/wheat/Wheat.java
@@ -1,8 +1,9 @@
 package org.homesteadhearts.entities.crops.wheat;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.DynamicCompositeEntity;
 
-public class Wheat {
+public class Wheat extends DynamicCompositeEntity {
     private WheatSprite wheatSprite;
 
     public Wheat (Coordinate2D location) {
@@ -10,10 +11,16 @@ public class Wheat {
         super();
     }
 
+    @Override
+    protected void setupEntities() {
+
+    }
+
     public void placeWheat(Coordinate2D location){
         wheatSprite = new WheatSprite(location);
 
     }
+
 
     public WheatSprite getWheatSprite(){
         return wheatSprite;
