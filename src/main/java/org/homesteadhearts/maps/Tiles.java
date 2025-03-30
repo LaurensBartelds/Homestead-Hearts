02e8e031f7727711e06805f15be5b46ed5976767
LaurensBartelds <laurensbartelds@protonmail.com>
2025-03-30 19:05:27 +0200

    Refactor GameLevelMap to use Tiles class and implement layer merging for improved map rendering

--- src/main/java/org/homesteadhearts/maps/Tiles.java
+++ src/main/java/org/homesteadhearts/maps/Tiles.java
@@ -4,9 +4,9 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.impl.SpriteEntity;
 
-public class Ground extends SpriteEntity {
+public class Tiles extends SpriteEntity {
 
-    public Ground(Coordinate2D initialPosition, Size size, String image) {
+    public Tiles(Coordinate2D initialPosition, Size size, String image) {
         super(image, initialPosition, size);
     }
 }
