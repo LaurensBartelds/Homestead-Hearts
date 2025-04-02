a6c6a556d911efb2313ce5262b1a2d924aa9afdd
LaurensBartelds <laurensbartelds@protonmail.com>
2025-04-02 22:27:26 +0200

    Remove Carrot entity from GameLevel initialization

--- src/main/java/org/homesteadhearts/scenes/GameLevel.java
+++ src/main/java/org/homesteadhearts/scenes/GameLevel.java
@@ -3,7 +3,6 @@ package org.homesteadhearts.scenes;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.UpdateExposer;
-import com.github.hanyaeger.api.entities.YaegerEntity;
 import com.github.hanyaeger.api.scenes.ScrollableDynamicScene;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
 import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
@@ -12,7 +11,6 @@ import javafx.scene.paint.Color;
 import org.homesteadhearts.entities.GUI.Hotbar;
 import org.homesteadhearts.entities.GUI.coins.CoinsTest;
 import org.homesteadhearts.entities.animals.bunny.Bunny;
-import org.homesteadhearts.entities.crops.carrot.Carrot;
 import org.homesteadhearts.entities.crops.seed.Seed;
 import org.homesteadhearts.entities.people.player.Player;
 import org.homesteadhearts.maps.GroundLayerMap;
@@ -21,10 +19,8 @@ import org.homesteadhearts.maps.tiles.TileManager;
 
 public class GameLevel extends ScrollableDynamicScene implements UpdateExposer, TileMapContainer, MouseButtonPressedListener {
     private TopLayerMap topLayerMap;
-    private Bunny bunny;
     private Player player;
     private GroundLayerMap groundLayerMap;
-    private TileManager tileManager;
 
     public GameLevel() {
     }
@@ -41,14 +37,11 @@ public class GameLevel extends ScrollableDynamicScene implements UpdateExposer, 
         // Create maps first
         groundLayerMap = new GroundLayerMap();
         topLayerMap = new TopLayerMap();
-        tileManager = new TileManager(groundLayerMap, topLayerMap);
+        TileManager tileManager = new TileManager(groundLayerMap, topLayerMap);
 
         // Add entities
-        bunny = new Bunny(new Coordinate2D(1000, 1000));
+        Bunny bunny = new Bunny(new Coordinate2D(1000, 1000));
         addEntity(bunny);
-
-        Carrot carrot = new Carrot(new Coordinate2D(1000, 1000));
-        addEntity(carrot);
 
         player = new Player(new Coordinate2D(1000, 1000), 500);
         player.setTileManager(tileManager);
