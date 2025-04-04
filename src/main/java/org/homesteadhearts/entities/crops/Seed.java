26726933c7f4517128172e441048203807133fb2
LaurensBartelds <laurensbartelds@protonmail.com>
2025-04-04 12:15:36 +0200

    Remove unused cropType variable from Seed class; ensure consistent formatting in Corn class

--- src/main/java/org/homesteadhearts/entities/crops/Seed.java
+++ src/main/java/org/homesteadhearts/entities/crops/Seed.java
@@ -6,12 +6,10 @@ import org.homesteadhearts.entities.tools.Tool;
 
 public class Seed extends Tool {
 
-    private final Class<? extends Crops> cropType;
     private final int SPRITE_INDEX;
 
     public Seed(String cropName, Class<? extends Crops> cropType, int SPRITE_INDEX, int slotNumber) {
         super(cropName + " Seeds", "Plant " + cropName + " crops", 1, slotNumber, "Seeds");
-        this.cropType = cropType;
         this.SPRITE_INDEX = SPRITE_INDEX;
     }
 
@@ -21,8 +19,6 @@ public class Seed extends Tool {
         addEntity(seedBagSprite);
     }
 
-    public Class<? extends Crops> getCropType() {
-        return cropType;
-    }
+
 
 }
